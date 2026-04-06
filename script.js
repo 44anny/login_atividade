40d7f397f39f98df41b4117cfbe3495690987f85
44anny <anavecer44@gmail.com>
2026-04-06 11:40:17 -0400

    firebase
    atualizae

---
+++ script.js
@@ -0,0 +1,31 @@
+//configuração Firebase
+
+const firebaseConfig = {
+
+    apikey:"suas credencias",
+    autDomain:"seu dominio",
+    projectId:"o nome projetodo firebase",
+);
+
+//inicializar
+
+firabase.initializeApp(firebaseeConfig)
+const auth = firebaseeConfig.auth();
+ //função login
+ function login()
+ const email = document.getElementById("email").value;
+ const senha= document.getElementById("senha").value;
+ const msg= document.getElementById("mensagem").value;
+
+ auth.signInwitEmailAndPassword(email,senha)
+ .then(()=>{
+    msg. innerText="Login realizado com Sucesso!"
+    msg.style.color="green";
+
+ })
+ .catch(console.error=>{
+    msg.innerText=error.mensagem
+    msg.style.color="red"
+ });
+
+};
